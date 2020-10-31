99f7894aaaf886cfeed303a0ec0bc6c49b8b54b3
ahmedsamir96m <ahmedsamir96m@gmail.com>
2020-10-31 14:01:11 +0200

    build the app base, created the basic state, created the add and delete functions

--- todo/src/App.js
+++ todo/src/App.js
@@ -5,9 +5,32 @@ import Todos from "./Components/Todos/Todos";
 const App = (props) => {
   const [todosList, setTodosList] = useState("");
 
+  const addTodoItem = () => {
+    const ma = Math.random() * 10;
+    setTodosList([
+      ...todosList,
+      {
+        item: `item number ${ma}`,
+        desc: `new item description ma`,
+        id: ma,
+      },
+    ]);
+  };
+
+  const deleteTodoItem = (id) => {
+    const newTodos = todosList.filter((todo) => {
+      return todo.id !== id;
+    });
+    setTodosList(newTodos);
+  };
+
   return (
     <div className="App">
-      <Todos />
+      <Todos
+        todos={todosList}
+        addTodoItem={addTodoItem}
+        deleteTodoItem={deleteTodoItem}
+      />
     </div>
   );
 };
