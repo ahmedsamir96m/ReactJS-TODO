9567d7543300013f98ea82b629b0b9ef4f7924e9
ahmedsamir96m <ahmedsamir96m@gmail.com>
2020-10-31 14:00:18 +0200

    build the app base, created the basic state, created the add and delete functions

--- todo/src/Components/Todos/Todos.js
+++ todo/src/Components/Todos/Todos.js
@@ -1,10 +1,45 @@
 import React from "react";
 
 const Todos = (props) => {
+  const { todos, addTodoItem, deleteTodoItem } = props;
   return (
     <div className="todos">
-      <h1>Hello</h1>
-      <h2>Todos</h2>
+      {todos.length >= 1 ? (
+        <div className="todos--container">
+          <h1>Hello</h1>
+          <h2>Todos</h2>
+          <div>
+            <button className="todos--add" onClick={addTodoItem}>
+              Add new item
+            </button>
+            {todos.map((todo) => {
+              console.log(todo);
+              return (
+                <div className="todo--item" key={todo.id}>
+                  <div>{todo.item}</div>
+                  <div>{todo.desc}</div>
+                  <div>{todo.id}</div>
+                  <button
+                    className="todos--delete"
+                    onClick={() => {
+                      deleteTodoItem(todo.id);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h1>No Todos</h1>
+          <button className="todos--add" onClick={addTodoItem}>
+            Add new item
+          </button>
+        </div>
+      )}
     </div>
   );
 };
